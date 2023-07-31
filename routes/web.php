cafc5d71ac0e105526b6e22cbaa6ee9f0bf3edef
HamzaImran54 <hamza.imran@softwarealliance.dk>
2023-07-31 17:11:10 +0500

    product prefix route

--- routes/web.php
+++ routes/web.php
@@ -47,11 +47,14 @@ Route::controller(CategoryController::class)->prefix('category')->group(function
 });
 
 //Product Routes
-Route::controller(ProductController::class)->prefix('product')->group(function(){
-    Route::get('/','productList')->name('product.list');
-    Route::post('/create', 'create')->name('product.create');
-    Route::get('/add', 'index')->name('product.add');
-    Route::get('/specific/data/{id?}', 'specificProductList')->name('specific.product.data');
+
+Route::prefix('product')->group(function(){
+    Route::controller(ProductController::class)->group(function(){
+        Route::get('/','productList')->name('product.list');
+        Route::post('/create', 'create')->name('product.create');
+        Route::get('/add', 'index')->name('product.add');
+        Route::get('/specific/data/{id?}', 'specificProductList')->name('specific.product.data');
+    });
 });
 
 require __DIR__.'/auth.php';
